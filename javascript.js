643cc4388b9375a61c14cc9ef2be22ecc656a572
matthew-dean-brown <mattrewbrown@gmail.com>
2023-08-23 10:34:51 +0200

    Completed math functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,26 @@
 var variable = "Any value I've set";
 
-var array = array[1,2,3,4,5,6,7,8,9,10]
-console.log(variable)
+var array = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(2*4)
+
+let meme = ("Meme")
+
+let marks = 20
+let score = 40.56
+let temp = -5.5
+console.log(temp)
+
+let phy = 78
+let maths = 72
+let chem = 89
+let avg = (phy+chem+maths)/3
+
+console.log(avg)
+
+let id = Math.random()*10
+let new_id = Math.round(id)
+console.log(new_id)
+
+console.log(Math.max(100,200,100,250))
+console.log(Math.min(100,200,100,250))
