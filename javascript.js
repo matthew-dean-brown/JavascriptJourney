9b8e48b2988c23366008f5be802a2fb59efbf414
matthewbrownza <matthewbrownza@gmail.com>
2023-08-27 23:01:59 +0200

    coding challenge discount price and area of circle

--- javascript.js
+++ javascript.js
@@ -1,28 +1,30 @@
-// var variable = "Any value I've set";
+/* 
+var variable = "Any value I've set";
 
-// var array = [1,2,3,4,5,6,7,8,9,10]
-// console.log(variable)
+var array = [1,2,3,4,5,6,7,8,9,10]
+console.log(variable)
 
-// let a
-// a = 10
-// let b
-// b = 20
-// let c = null
-// console.log(c)
+let a
+a = 10
+let b
+b = 20
+let c = null
+console.log(c)
 
-// const price = 100
+const price = 100
 
-// console.log(2*Math.PI*10)
+console.log(2*Math.PI*10)
 
-// console.log(Math.abs(20))
+console.log(Math.abs(20))
 
-// console.log(Math.pow(7,2))
+console.log(Math.pow(7,2))
 
-// //rounds down number
-// console.log(Math.floor(11.8))
+//rounds down number
+console.log(Math.floor(11.8))
 
-// console.log(Math.ceil(20.1))
-
+console.log(Math.ceil(20.1))
+*/
+/*
 //template literal
 let a = `Apple`
 
@@ -34,4 +36,27 @@ let b = `The total value is ${10+2}`
 console.log(b)
 
 let c = `The total is ${6+2} and the average is ${(3+3+2)/2} `
-console.log(c)
+console.log(typeof(a))
+
+//type conversion
+let salary = `1000`
+parseInt(salary)+100
+//parseFloat() for decimals
+console.log(parseInt(salary)+100)
+*/
+
+// coding challenge: Discount price
+let listPrice
+let discountPercentage 
+
+listPrice = 200
+discountPercentage = 10
+
+
+console.log(listPrice *(100-discountPercentage)/100 )
+
+ //Area of a circle
+
+radius = 50 
+let Area = parseInt(Math.PI * radius**2)
+console.log(`The are of a circle with the radius ${radius} is ${Area} squre meters`)
