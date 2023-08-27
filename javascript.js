6b25c3d57597dcbdcdac755b99ae42436e428d51
matthewbrownza <matthewbrownza@gmail.com>
2023-08-27 22:24:31 +0200

    Template literals

--- javascript.js
+++ javascript.js
@@ -1,4 +1,37 @@
-var variable = "Any value I've set";
+// var variable = "Any value I've set";
 
-var array = array[1,2,3,4,5,6,7,8,9,10]
-console.log(variable)
+// var array = [1,2,3,4,5,6,7,8,9,10]
+// console.log(variable)
+
+// let a
+// a = 10
+// let b
+// b = 20
+// let c = null
+// console.log(c)
+
+// const price = 100
+
+// console.log(2*Math.PI*10)
+
+// console.log(Math.abs(20))
+
+// console.log(Math.pow(7,2))
+
+// //rounds down number
+// console.log(Math.floor(11.8))
+
+// console.log(Math.ceil(20.1))
+
+//template literal
+let a = `Apple`
+
+let name = "John"
+
+console.log(`Welcome to our website ${name}`)
+
+let b = `The total value is ${10+2}`
+console.log(b)
+
+let c = `The total is ${6+2} and the average is ${(3+3+2)/2} `
+console.log(c)
