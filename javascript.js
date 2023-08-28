c40482bbb00e0ea0d7817515fe075202952ea0f5
matthew-dean-brown <mattrewbrown@gmail.com>
2023-08-28 14:18:09 +0200

    color.css added

--- javascript.js
+++ javascript.js
@@ -1,52 +1,60 @@
-var variable = "Any value I've set";
+// var variable = "Any value I've set";
 
-//array
-var array = [1,2,3,4,5,6,7,8,9,10]
+// //array
+// var array = [1,2,3,4,5,6,7,8,9,10]
 
-console.log(2*4)
+// console.log(2*4)
 
-let meme = ("Meme")
+// let meme = ("Meme")
 
-let marks = 20
-let score = 40.56
-let temp = -5.5
-console.log(temp)
+// let marks = 20
+// let score = 40.56
+// let temp = -5.5
+// console.log(temp)
 
-//learning +, -, x, /divide
-let phy = 78
-let maths = 72
-let chem = 89
-let avg = (phy+chem+maths)/3
+// //learning +, -, x, /divide
+// let phy = 78
+// let maths = 72
+// let chem = 89
+// let avg = (phy+chem+maths)/3
 
-console.log(avg)
+// console.log(avg)
 
-//learning built in maths functions
-let id = Math.random()*10
-let new_id = Math.round(id)
-console.log(new_id)
+// //learning built in maths functions
+// let id = Math.random()*10
+// let new_id = Math.round(id)
+// console.log(new_id)
 
-console.log(Math.max(100,200,100,250))
-console.log(Math.min(100,200,100,250))
+// console.log(Math.max(100,200,100,250))
+// console.log(Math.min(100,200,100,250))
 
-let hello = "Hello"
-let world = "world"
-let name = "Matthew"
-let last_name = "Brown"
+// let hello = "Hello"
+// let world = "world"
+// let name = "Matthew"
+// let last_name = "Brown"
 
-//replcing string info with other text
-var newName = name.replace("M", "f")
-console.log(hello + ' ' + world )
-console.log(name.length)
-console.log(newName.toUpperCase() + ' ' + last_name )
+// //replcing string info with other text
+// var newName = name.replace("M", "f")
+// console.log(hello + ' ' + world )
+// console.log(name.length)
+// console.log(newName.toUpperCase() + ' ' + last_name )
 
-let a = "computer"
-//determining last indexing
-console.log(a.length - 1)
-//pulling certain string information from variable
-console.log(a.substring(0,4))
-console.log(a.slice(0,4))
+// let a = "computer"
+// //determining last indexing
+// console.log(a.length - 1)
+// //pulling certain string information from variable
+// console.log(a.substring(0,4))
+// console.log(a.slice(0,4))
 
-let names ="John, Rob, Tim, Newton"
-// seperates string into array
-array_names = names.split(',')
-console.log(array_names[1])
+// let names ="John, Rob, Tim, Newton"
+// // seperates string into array
+// array_names = names.split(',')
+// console.log(array_names[1])
+
+
+
+var Matthew = "Mathematics is my favourite subject"
+
+var num1 = 3   
+var num2 = 6
+console.log(num1 / num2*num1)
